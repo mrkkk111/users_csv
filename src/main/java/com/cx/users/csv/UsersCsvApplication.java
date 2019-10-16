5ff3972d2cf1ab173d2e034ceb3fc37c7c865df6
kornel <masterkkk@o2.pl>
2019-10-16 23:59:12 +0200

    initial commit

---
+++ src/main/java/com/cx/users/csv/UsersCsvApplication.java
@@ -0,0 +1,13 @@
+package com.cx.users.csv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersCsvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersCsvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cx/users/csv/UsersCsvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cx.users.csv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersCsvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
