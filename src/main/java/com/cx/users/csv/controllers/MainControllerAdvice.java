5af97ca91b7a41b8526f69815147d0c989d05758
kornel <masterkkk@o2.pl>
2019-10-18 10:10:47 +0200

    Add error handling for search box

--- src/main/java/com/cx/users/csv/controllers/MainControllerAdvice.java
+++ src/main/java/com/cx/users/csv/controllers/MainControllerAdvice.java
@@ -2,8 +2,10 @@ package com.cx.users.csv.controllers;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 
 
@@ -11,6 +13,7 @@ import org.springframework.web.context.request.WebRequest;
 public class MainControllerAdvice {
     private Logger logger = LoggerFactory.getLogger(MainControllerAdvice.class);
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler({Exception.class})
     public final String handleException(Exception e, WebRequest req) {
         logger.error("Unhandled exception: " + e.getMessage());
